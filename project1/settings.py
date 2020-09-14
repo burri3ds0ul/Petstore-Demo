f5ba107313a954f33783d949411b3952347e9dcd
Nabin Khanal <nabinkhanal128@gmail.com>
2020-09-14 21:31:50 +0545

    Try Lol

--- project1/settings.py
+++ project1/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = Path('C:/Users/lenovo/Desktop/project1/static/').resolve()
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = Path('C:/Users/lenovo/Desktop/project1/media/').resolve()
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
